f946b42dbd6734c0e3d567a2e7811c5475e666e9
miadong <dong.xinm@husky.neu.edu>
2019-10-15 17:40:08 -0400

    test

--- aeroplane/assets/js/aeroplane.jsx
+++ aeroplane/assets/js/aeroplane.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Stage, Layer, Circle } from 'react-konva';
 import _ from "lodash";
 // import { ImageBackground } from 'react-native';
 
@@ -13,7 +14,11 @@ class Aeroplane extends React.Component {
 
     // this.channel = props.channel;
     this.state = {
+      // yellow, blue, red, green
+      pieces_loc: [{x: 100, y: 700},]
     };
+
+    let R = 5;
 
     // this.channel
     //     .join()
@@ -21,23 +26,27 @@ class Aeroplane extends React.Component {
     //     .receive("error", resp => { console.log("Unable to join", resp); });
   }
 
+  move(pp) {
+    let pieces = _.map(this.state.pieces_loc, (piec, ii) =>
+      (ii == pp ? _.assign({}, piec, {x: piec.x + 10, y: piec.y + 10}) : piec));
+    this.setState(_.assign({}, this.state, {pieces}));
+  }
+
   render() {
     // let bg = require('/Users/elephant/web-dev-git/aero-img/plain-board.jpg');
     // let imageFilePath = '/Users/elephant/web-dev-git/aero-img/plain-board.jpg';
+    let pieces = _.map(this.state.pieces_loc, (pp, ii) =>
+      <Circle key={ii} radius={R} x={pp.x} y={pp.y} fill="yellow"
+              onClick={() => this.move(ii)} />);
       
     return(
         <div className="background">
+          <Stage>
+            <Layer>
+              {pieces}
+            </Layer>
+          </Stage>
           <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-          <div><p>here we go!</p></div>
-        {/* <div className='bg' style ={ {backgroundImage: `url("+bg+")` }}></div> */}
         </div>
     );
   }
